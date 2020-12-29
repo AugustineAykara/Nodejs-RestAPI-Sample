b9a82cf1b1e761aaa511652257dd6f7b1054349f
AugustineAykara <augustineaykara@gmail.com>
2020-12-30 00:14:52 +0530

    handling GET request

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
+const e = require('express');
 const express = require('express')
 const app = express()
 
+const people = [
+    {
+        id: 1,
+        name: "Abey"
+    },
+    {
+        id: 2,
+        name: "Abin"
+    },
+    {
+        id: 3,
+        name: "Akhil"
+    },
+]
+
+
+
 app.get('/', (req, res) => {
-    res.send("Hello new user")
+    res.send("Hello new user...")
 });
 
-app.listen(3000, () => {
-    console.log("server on port 3000");
+app.get('/api/people', (req, res) => {
+    res.send(people)
+});
+
+app.get('/api/people/:id', (req, res) => {
+    const person = people.find(c => c.id === parseInt(req.params.id))
+    if(!person) res.status(404).send("Person with given id not found")
+    res.send(person)
+});
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`server on port ${port}`);
 })
